1ac73408c6a211dfddda45dd0f9ec6fbb324c6d7
Patrik <purgai.patrik@gmail.com>
2019-02-24 11:31:36 +0100

    commenting and documentation added

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# pycin
-API for the Hungarian Cinema City.
+# Python API for the Hungarian Cinema City
+
+## Description
+
+Each query has to originate from a `_CinemaQueryResult` object, which can be obtained by
+either `fetch_cinemas()` or `fetch_common_cinemas()` method. The latter only lists the most popular cinemas of Budapest. By calling `_CinemaQueryResult.fetch_events()` the returned `_EventQueryResult` object contains the events of the cinames in that particular `_CinemaQueryResult` object. To obtain the cinemas or the events in the cinemas, call `_QueryResult.select()`. `_QueryResult.where()` method applies a filter on the object and returns a new one, based on a predicate function.
+
+## Example usage
+
+Fetching the available cinemas.
+
+```python
+
+from pycin import fetch_cinemas
+
+
+cinemas = fetch_cinemas()
+
+print(cinemas.select())
+```
+
+Output:
+
+```
+[Cinema(id='1124', name='Alba - Székesfehérvár'), Cinema(id='1133', name='Allee - Budapest'), Cinema(id='1132', name='Aréna - Budapest'), Cinema(id='1131', name='Balaton - Veszprém'), Cinema(id='1139', name='Campona - Budapest'), Cinema(id='1127', name='Debrecen'), Cinema(id='1141', name='Duna Pláza - Budapest'), Cinema(id='1125', name='Győr'), Cinema(id='1129', name='Miskolc'), Cinema(id='1143', name='Nyíregyháza'), Cinema(id='1128', name='Pécs'), Cinema(id='1134', name='Savaria - Szombathely'), Cinema(id='1136', name='Sopron'), Cinema(id='1126', name='Szeged'), Cinema(id='1130', name='Szolnok'), Cinema(id='1137', name='Westend - Budapest'), Cinema(id='1135', name='Zalaegerszeg')]
+```
+
+If there are no arguments provided for the `select()` method it returns the `_cinemas` field. The output of this function can be customized by feeding the name of the parameters of the `Cinema` namedtuple objects.
+
+```python
+print(cinemas.select('name'))
+```
+
+Output:
+
+```
+[('Alba - Székesfehérvár',), ('Allee - Budapest',), ('Aréna - Budapest',), ('Balaton - Veszprém',), ('Campona - Budapest',), ('Debrecen',), ('Duna Pláza - Budapest',), ('Győr',), ('Miskolc',), ('Nyíregyháza',), ('Pécs',), ('Savaria - Szombathely',), ('Sopron',), ('Szeged',), ('Szolnok',), ('Westend - Budapest',), ('Zalaegerszeg',)]
+```
+
+Fetching the events in the cinema *Alle*, where the screened movie is a horror.
+
+```python
+
+from pycin import ALLE
+
+result = (
+    cinemas.where(lambda c: c.id == ALLE.id)
+        .fetch_events()
+        .where(lambda e: 'horror' in e.movie.attributes)
+        .movies
+)
+
+print(result)
+```
+
+Outputs:
+
+```
+[Movie(id='3208d2r', name='Boldog Halálnapot 2', attributes=['16-plus', '2d', 'dubbed', 'dubbed-lang-hu', 'horror', 'mystery', 'thriller'], length=100), Movie(id='3265d2r', name='A csodagyerek', attributes=['16-plus', '2d', 'dubbed', 'dubbed-lang-hu', 'horror', 'thriller'], length=92)]
+```
--- pycin.py
+++ pycin.py
@@ -1,10 +1,16 @@
 """
 
+@author:    Patrik Purgai
+@copyright: Copyright 2019, pycin
+@license:   MIT
+@email:     purgai.patrik@gmail.com
+@date:      2019.02.24.
 """
 
 import requests
 import time
 import json
+import logging
 
 from collections import namedtuple
 
@@ -12,11 +18,21 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 DATA_API_URL = 'https://www.cinemacity.hu/hu/' \
                'data-api-service/v1/quickbook/10102/'
 DATE_FORMAT = '%Y-%m-%d'
 EVENT_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S'
-DEFAULT_LANG = 'hu'
+DEFAULT_LANG = 'en'
 
 
 Movie = namedtuple('Movie', ['id', 'name', 'attributes', 'length'])
@@ -51,78 +67,226 @@ CAMPONA = Cinema('1139', 'Campona - Budapest')
 WESTEND = Cinema('1137', 'Westend - Budapest')
 
 
-class EventQuery:
+def today():
+    """Convenience function for getting the current
+    date in datetime format."""
+    return datetime.fromtimestamp(time.mktime(time.localtime()))
+
+
+def add_days(date, days):
+    """Convenience function for adding days to a datetime object."""
+    return date + relativedelta(days=days)
+
+
+def fetch_common_cinemas(lang=DEFAULT_LANG):
+    """Convenience function for fetching the most common cinemas."""
+    return _CinemaQueryResult([ALLE, ARENA, CAMPONA, WESTEND], lang)
+
+
+def create_cinema_query(cinema, lang=DEFAULT_LANG):
+    """Convenience function for fcreating a query with known cinemas."""
+    if not isinstance(cinema, list):
+        cinema = [cinema]
+    return _CinemaQueryResult(cinema, lang)
+
+
+class _QueryResult:
+
+    def where(self, predicate):
+        """Selects the subset of the querried elements, 
+        based on the provided predicate function.
+        
+        Arguments:
+            predicate: function, that returns bool value.
+        """
+        raise NotImplementedError('Subclasses must implement this.')
+
+    def select(self, *attributes):
+        """Lists the event attribute values, which are present 
+        in the query.
+        
+        Arguments:
+            attributes: name of the namedtuple attributes,
+                which are returned in the list. Defaults to all
+                attributes.
+
+        Returns:
+            A list of tuples, which are the values of the `Event`
+            namedtuple attributes. If the `attributes` parameter
+            is provided, the order of the values in the tuples is
+            the same as the provided attributes names. By default
+            the namedtuple objects are returned.
+        """
+        raise NotImplementedError('Subclasses must implement this.')
+
+
+class _EventQueryResult(_QueryResult):
     """Stores the query result for the events of a cinema."""
 
     def __init__(self, cinemas, movies, events):
+        """Creates and _EventQueryResult object.
+        
+        Arguments:
+            cinemas: a list of `Cinema` namedtuple objects, 
+                which are present in the events.
+            movies: a list of `Movie` namedtuple objects,
+                which are present in the events.
+            events: a list of `Event` namedtuple objects.
+        """
         self._movies = movies
         self._events = events
         self._cinemas = cinemas
 
-    def select_events(self, event_predicate=lambda event: True):
-        events = [event for event in self._events if event_predicate(event)]
+    def where(self, predicate=lambda event: True):
+        """Creates a new `_EventQueryResult` object from the cinemas,
+        which are seleceted by the predicate function.
+        
+        Arguments:
+            predicate: function, that receives a `Event` 
+                namedtuple object and returns a bool value.
+        
+        Returns:
+            The new `_EventQueryResult` object with the selected
+            events.
+        """
+        events = [e for e in self._events if predicate(e)]
         movies, cinemas = {}, {}
         for event in events:
             movies[event.movie.id] = event.movie
             cinemas[event.cinema.id] = event.cinema
-        return EventQuery(list(cinemas.values()), list(movies.values()), events)
+        return _EventQueryResult(
+            list(cinemas.values()), list(movies.values()), events)
 
-    def movies(self, *attributes):
-        if not attributes:
-            return self._movies
-        movies = []
-        for movie in self._movies:
-            movies.append(tuple(getattr(movie, attrib)
-                                for attrib in attributes))
+    def select(self, *attributes):
+        """Lists the event attribute values, which are present 
+        in the query.
+        
+        Arguments:
+            attributes: name of the `Event` namedtuple attributes,
+                which are returned in the list. Defaults to all
+                attributes.
 
-        return movies
-
-    def cinemas(self, *attributes):
-        if not attributes:
-            return self._cinemas
-        cinemas = []
-        for cinema in self._cinemas:
-            cinemas.append(tuple(getattr(cinema, attrib)
-                                 for attrib in attributes))
-
-        return cinemas
-    
-    def events(self, *attributes):
+        Returns:
+            A list of tuples, which are the values of the `Event`
+            namedtuple attributes. If the `attributes` parameter
+            is provided, the order of the values in the tuples is
+            the same as the provided attributes names. By default
+            the namedtuple objects are returned.
+        """
         if not attributes:
             return self._events
-        events = []
-        for event in self._events:
-            events.append(tuple(getattr(event, attrib)
-                                for attrib in attributes))
+        try:
+            res = [
+                tuple(getattr(ev, attr) for attr in attributes)
+                for ev in self._events
+            ]
+        except AttributeError as e:
+            msg = '{msg}. Available attributes are {attribs}'.format(
+                msg=e, attribs=','.join(Event._fields))
+            raise AttributeError(msg)
 
-        return events
+        return res
+
+    @property
+    def movies(self):
+        return self._movies
+
+    @property
+    def cinemas(self):
+        return self._cinemas
     
 
-class CinemaQuery:
+class _CinemaQueryResult(_QueryResult):
+    """Stores the query result for the cinemas."""
 
     def __init__(self, cinemas, lang=DEFAULT_LANG):
+        """Creates a cinema query object.
+        
+        Arguments:
+            cinemas: list, containg the 
+        """
         self._lang = lang
         self._cinemas = cinemas
 
-    def select_cinemas(self, cinema_predicate=lambda cinema: True):
-        return CinemaQuery([c for c in self._cinemas
-                            if cinema_predicate(c)], self._lang)
+    def where(self, predicate=lambda cinema: True):
+        """Creates a new `_CinemaQueryResult` object from the cinemas,
+        which are seleceted by the predicate function.
+        
+        Arguments:
+            predicate: function, that receives a `Cinema` 
+                namedtuple object and returns a bool value.
+        
+        Returns:
+            The new `_CinemaQueryResult` object with the selected
+            cinemas.
+        """
+        return _CinemaQueryResult([c for c in self._cinemas
+                            if predicate(c)], self._lang)
     
-    def query_events(self, start_date, end_date=None):
+    def select(self, *attributes):
+        """Lists the cinema attribute values, which are present 
+        in the query.
+        
+        Arguments:
+            attributes: name of the `Cinema` namedtuple attributes,
+                which are returned in the list. Defaults to all
+                attributes.
+        
+        Returns:
+            A list of tuples, which are the values of the `Cinema`
+            namedtuple attributes. If the `attributes` parameter
+            is provided, the order of the values in the tuples is
+            the same as the provided attributes names. By default
+            the namedtuple objects are returned.
+        """
+        if not attributes:
+            return self._cinemas
+        try:
+            res = [
+                tuple(getattr(cin, attr) for attr in attributes)
+                for cin in self._cinemas
+            ]
+        except AttributeError as e:
+            msg = '{msg}. Available attributes are {attribs}'.format(
+                msg=e, attribs=','.join(Cinema._fields))
+            raise AttributeError(msg)
+
+        return res
+
+    def fetch_events(self, start_date=None, end_date=None):
+        """Queries the events for the cinemas, which are present
+        in the `_CinemaQueryResult` object.
+        
+        Arguments:
+            start_date: datetime object, that marks the beginning
+                of the time interval for the querried event dates.
+                Defaults to today.
+            end_date: datetime object, which is the end of the time
+                interval for the events in the query.
+
+        Returns:
+            `_EventQueryResult` type objects, that holds the requested events.
+        """
+        if start_date is None:
+            start_date = today()
         date = start_date
         end_date = end_date if end_date else date
         assert date <= end_date, 'start_date must be lesser' \
-                                 ' or equal to end_date'
+                                    ' or equal to end_date'
         movies = {}
         events = []
 
         while date <= end_date:
             for cinema in self._cinemas:
                 date_string = datetime.strftime(date, DATE_FORMAT)
+
+                logger.info('Fetching events for {id} on {date}.'.format(
+                    id=cinema.id, date=date_string))
+
                 request_url = '{url}/film-events/in-cinema/{cid}/' \
-                              'at-date/{date}?attr=&lang={lang}'.format(
-                               url=DATA_API_URL, cid=cinema.id,
-                               date=date_string, lang=self._lang)
+                                'at-date/{date}?attr=&lang={lang}'.format(
+                                url=DATA_API_URL, cid=cinema.id,
+                                date=date_string, lang=self._lang)
                 response = requests.get(request_url)
                 data = json.loads(response.text)['body']
                 
@@ -137,25 +301,29 @@ class CinemaQuery:
                         event['id'], event['bookingLink'],
                         movies[event['filmId']],
                         cinema, datetime.strptime(
-                          event['eventDateTime'], EVENT_DATE_FORMAT),
+                            event['eventDateTime'], EVENT_DATE_FORMAT),
                         event['soldOut']))
 
                 time.sleep(0.2)
 
             date = date + relativedelta(days=1)
-        return EventQuery(self._cinemas, list(movies.values()), events)
 
-    def cinemas(self, *attributes):
-        if not attributes:
-            return self._cinemas
-        cinemas = []
-        for cinema in self._cinemas:
-            cinemas.append(tuple(getattr(cinema, attrib)
-                                 for attrib in attributes))
-        return cinemas
+        logger.info('Event query created with {size} elements.'.format(
+            size=len(events)))
+        
+        return _EventQueryResult(
+            self._cinemas, list(movies.values()), events)
 
 
-def query_cinemas(lang=DEFAULT_LANG, until_date=None):
+def fetch_cinemas(lang=DEFAULT_LANG, until_date=None):
+    """Queries the Cinema City cinemas.
+    
+    Arguments:
+        lang: str, language of the query result. Can be `en` or `hu`.
+    
+    Returns:
+        `_CinemaQueryResult` type object, which holds the querried cinemas.
+    """
     if not until_date:
         until_date = datetime.fromtimestamp(
             time.mktime(time.localtime())) + relativedelta(years=1)
@@ -170,4 +338,8 @@ def query_cinemas(lang=DEFAULT_LANG, until_date=None):
     response = requests.get(request_url)
     data = json.loads(response.text)['body']['cinemas']
     cinemas = [Cinema(c['id'], c['displayName']) for c in data]
-    return CinemaQuery(cinemas, lang)
+
+    logger.info('Cinema query created with {size} elements.'.format(
+        size=len(cinemas)))
+
+    return _CinemaQueryResult(cinemas, lang)
